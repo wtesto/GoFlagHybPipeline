e3680e6c370f0c7f8e115d60c3291da40d1b7202
Weston Testo <63684485+wtesto@users.noreply.github.com>
2023-04-15 10:57:51 -0500

    Update select_samples.sh

--- select_samples.sh
+++ select_samples.sh
@@ -4,7 +4,7 @@
 ###Make a .txt file names "sample_list.txt" with the plate and well numbers of your samples (e.g., P017_WB05)
 ###Include one sample per line
 ###Selected samples will be placed in a new directory called "selected_samples"
-###You can then move those samples to the directory where you will perform your SECAPR analyses
+###You can then move those samples to the directory where you will perform your HybPiper analyses
 
 ls -1 RAPiD* | grep -f sample_list.txt > subset.txt
 
